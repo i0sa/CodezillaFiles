f95e8a1674118577e9e9596cd2e42d201a9cd68b
i0sa <me@i0sa.com>
2020-10-13 22:14:04 +0200

    Create UIStackView+Extensions.swift

---
+++ UIStackView+Extensions.swift
@@ -0,0 +1,14 @@
+extension UIStackView {
+    func removeAllArrangedSubviews() {
+        let removedSubviews = arrangedSubviews.reduce([]) { (allSubviews, subview) -> [UIView] in
+            self.removeArrangedSubview(subview)
+            return allSubviews + [subview]
+        }
+        
+        // Deactivate all constraints
+        NSLayoutConstraint.deactivate(removedSubviews.flatMap({ $0.constraints }))
+        
+        // Remove the views from self
+        removedSubviews.forEach({ $0.removeFromSuperview() })
+    }
+}
