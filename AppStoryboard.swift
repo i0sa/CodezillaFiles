e9ed1dc160009d55e4a1f52a35a8e2990a91a9c6
i0sa <me@i0sa.com>
2020-10-15 16:31:45 +0200

    Create AppStoryboard.swift

---
+++ AppStoryboard.swift
@@ -0,0 +1,50 @@
+//
+//  AppStoryboards.swift
+//  AppStoryboards
+//
+//  Created by Gurdeep on 15/12/16.
+//  Copyright © 2016 Gurdeep. All rights reserved.
+//
+import Foundation
+import UIKit
+
+enum AppStoryboard : String {
+    
+    case Main
+    
+    var instance : UIStoryboard {
+        
+        return UIStoryboard(name: self.rawValue, bundle: Bundle.main)
+    }
+    
+    func viewController<T : UIViewController>(viewControllerClass : T.Type, function : String = #function, line : Int = #line, file : String = #file) -> T {
+        
+        let storyboardID = (viewControllerClass as UIViewController.Type).storyboardID
+         
+        guard let scene = instance.instantiateViewController(withIdentifier: storyboardID) as? T else {
+            
+            fatalError("ViewController with identifier \(storyboardID), not found in \(self.rawValue) Storyboard.\nFile : \(file) \nLine Number : \(line) \nFunction : \(function)")
+        }
+        
+        return scene
+    }
+    
+    func initialViewController() -> UIViewController? {
+        
+        return instance.instantiateInitialViewController()
+    }
+}
+
+extension UIViewController {
+    
+    // Not using static as it wont be possible to override to provide custom storyboardID then
+    class var storyboardID : String {
+        
+        return "\(self)"
+    }
+    
+    static func instantiate(fromAppStoryboard appStoryboard: AppStoryboard) -> Self {
+        
+        return appStoryboard.viewController(viewControllerClass: self)
+    }
+}
